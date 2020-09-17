6650f0b829268d1a6e9da7ed494a796481d29bf4
bywahjoe <67533574+bywahjoe@users.noreply.github.com>
2020-09-17 14:44:40 +0700

    Update mainarduino.py

--- mainarduino.py
+++ mainarduino.py
@@ -179,12 +179,12 @@ def startSerialKompas():
 def openKompas():
 	threadKompas = threading.Thread(target=startSerialKompas,daemon=True)
 	threadKompas.start()
-#openKompas()
+openKompas()
 drible()
-setMotor(150,150,150)
-while True:
+#setMotor(150,150,150)
+"""while True:
 	tendang()
-	time.sleep(7)
+	time.sleep(7)"""
 #print(getAllMyIR())
 #tendang()
 #PENENDANG.write(1)#AKTIF HIGH
